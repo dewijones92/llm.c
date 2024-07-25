4554e906d905c656d27525e454416e8e18dd8cfb
Your Name <you@example.com>
2024-07-25 06:56:27 +0100

    more

--- go.sh
+++ go.sh
@@ -1,2 +1,8 @@
+set -ex;
+
 make train_gpt2fp32cu
-./train_gpt2fp32cu
+
+# train on a single GPU
+./train_gpt2cu \
+    -i "dev/data/fineweb10B/fineweb_train_*.bin" \
+    -j "dev/data/fineweb10B/fineweb_val_*.bin" 
